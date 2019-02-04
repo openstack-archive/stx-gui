43e07f799cd7890f72cafa37324897b36ba66258
Al Bailey <Al.Bailey@windriver.com>
2019-02-04 14:03:01 -0600

    Remove calls to non existant novaclient imports
    The wrs_pci and wrs_providernet python files do
    not exist in novaclient.
    
    This change removes those imports.
    
    The GUI code that is processing the results from
    those imports is not currently been removed, as it may be
    used by upcoming enhancements to sysinv.
    
    Story: 2004751
    Task: 29357
    Change-Id: I39dfbdf60d9de008072253f1074fb2ee5c61df72
    Signed-off-by: Al Bailey <Al.Bailey@windriver.com>

--- starlingx-dashboard/starlingx-dashboard/starlingx_dashboard/api/nova.py
+++ starlingx-dashboard/starlingx-dashboard/starlingx_dashboard/api/nova.py
@@ -4,11 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-from novaclient.v2 import wrs_pci
-from novaclient.v2 import wrs_providernets
-
-from openstack_dashboard.api import base
-from openstack_dashboard.api.nova import nova_exceptions
 from openstack_dashboard.api.nova import novaclient
 
 
@@ -27,42 +22,3 @@ def server_group_create(request, name, project_id, metadata, policies):
 
 def server_group_delete(request, server_group_id):
     return novaclient(request).server_groups.delete(server_group_id)
-
-
-def provider_network_get(request, providernet_id):
-    return wrs_providernets.ProviderNetsManager(novaclient(request)).get(
-        providernet_id)
-
-
-class DeviceUsage(base.APIResourceWrapper):
-    """Wrapper for Inventory Device Usage"""
-    _attrs = ['device_id', 'device_name', 'vendor_id', 'class_id',
-              'pci_vfs_configured', 'pci_vfs_used',
-              'pci_pfs_configured', 'pci_pfs_used']
-
-
-def get_device_usage_list(request):
-    usages = wrs_pci.PciDevicesManager(novaclient(request)).list()
-    return [DeviceUsage(n) for n in usages]
-
-
-def get_device_usage(request, device_id):
-    if device_id is None:
-        raise nova_exceptions.ResourceNotFound
-
-    usage = wrs_pci.PciDevicesManager(novaclient(request)).list(
-        device=device_id)
-    return DeviceUsage(usage[0])
-
-
-class DetailUsage(base.APIResourceWrapper):
-    """Wrapper for Inventory Device Usage"""
-    _attrs = ['host',
-              'pci_vfs_configured', 'pci_vfs_used',
-              'pci_pfs_configured', 'pci_pfs_used']
-
-
-def get_detail_usage(request, device_id):
-    usages = wrs_pci.PciDevicesManager(novaclient(request)).get(
-        device_id)
-    return [DetailUsage(n) for n in usages]
